7d739ae28a428910f29e8fb8b2aba5bc1f166c3d
thiago aragao <thiagohoaragao@gmail.com>
2022-06-07 13:51:39 -0300

    Added Routes and fixed validation for cpf

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,59 @@
 class DocumentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @documents = policy_scope(document).order(created_at: :desc)
+    authorize @documents
+  end
+
+  def new
+    @document = Document.new
+    authorize @document
+  end
+
+  def create
+    @document = Document.new(document_params)
+    @document.user = current_user
+
+    authorize @document
+
+    if @document.save
+      redirect_to documents_path(@document)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @document = Document.find(params[:id])
+    authorize @document
+  end
+
+  def update
+    @document = Document.find(params[:id])
+    authorize @document
+    if @document.update(document_params)
+      redirect_to @document, notice: 'Seu documento foi atualizado!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
+    redirect_to documents_path(@document)
+    authorize @document
+  end
+
+  def show
+    @document = Document.find(params[:id])
+    authorize @document
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:category, :renewal, :renew_date, :user_id, :photo)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   has_one_attached :avatar
   validates :first_name, :last_name, :city, :state, :email, :birthday, :cpf, presence: true
   validates :cpf, :email, uniqueness: true
+  validate :validate_cpf
+  validates :validate_cpf, length: { is: 11 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def validate_cpf
+    if !cpf.valid_cpf?
+      errors.add(:cpf, "Invalido")
+    end
+  end
 end
