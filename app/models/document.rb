c8beb837678e65c94aa140b45ae235c6d3691184
igor martins <igordavillamartins@outlook.com>
2022-06-08 15:31:05 -0300

    fix navbar to user logged out, index, fixed form for create document

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,7 @@ class Document < ApplicationRecord
   belongs_to :user
   validates :category, presence: true
   validates :category, uniqueness: true
+  validates :category, inclusion: { in: ['CPF', 'Identidade', 'Habilitação', 'Documento do Veículo',
+                                         'Carteira de Vacinação', 'Passaporte', 'Outros'] }
   has_one_attached :photo
 end
