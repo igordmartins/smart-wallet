883071f80d89186d44b9973c0415a1f29b550f23
igor martins <igordavillamartins@outlook.com>
2022-06-08 11:42:19 -0300

    fixed create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,13 @@ class User < ApplicationRecord
   has_many :documents, dependent: :destroy
   has_one_attached :avatar
   validates :first_name, :last_name, :city, :state, :email, :birthday, :cpf, presence: true
-  validates :cpf, :email, uniqueness: true
+  validates :cpf, uniqueness: true
   validate :validate_cpf
-  validates :validate_cpf, length: { is: 15 }
+  validates :validate_cpf, length: { maximum: 14 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def validate_cpf
-    if !cpf.valid_cpf?
-      errors.add(:cpf, "Invalido")
-    end
+    errors.add(:cpf, "Inválido") unless cpf.valid_cpf?
   end
 end
