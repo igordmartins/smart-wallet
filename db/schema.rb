c42d2a52aa5809a2d61d02e7c916c8cb4275c0bf
igor martins <igordavillamartins@outlook.com>
2022-06-09 16:53:43 -0300

    remove city and state column of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_172604) do
+ActiveRecord::Schema.define(version: 2022_06_09_194426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,8 +63,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_172604) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "city"
-    t.string "state"
     t.date "birthday"
     t.string "cpf"
     t.index ["email"], name: "index_users_on_email", unique: true
