6bbf19335eb3c259edc444fa24bd086a10e1c2ad
igor martins <igordavillamartins@outlook.com>
2022-06-10 17:23:45 -0300

    index documents and document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ApplicationRecord
   belongs_to :user
   validates :category, presence: true
+  validates :category, uniqueness: { scope: :user }
   validates :category, inclusion: { in: ['Identidade', 'Habilitação', 'Documento do Veículo',
                                          'Carteira de Vacinação', 'Passaporte', 'Título de Eleitor', 'Outros'] }
   has_one_attached :front
