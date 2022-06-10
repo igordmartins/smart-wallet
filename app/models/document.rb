7606390c6a8b35d49c63c4576b731b5b3c739e01
mateus bittencourt <mateusmbittencourt@gmail.com>
2022-06-10 14:25:19 -0300

    correct layouts and image upload

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,8 +2,8 @@ class Document < ApplicationRecord
   belongs_to :user
   validates :category, presence: true
   validates :category, uniqueness: true
-  validates :category, inclusion: { in: ['CPF', 'Identidade', 'Habilitação', 'Documento do Veículo',
-                                         'Carteira de Vacinação', 'Passaporte', 'Outros'] }
+  validates :category, inclusion: { in: ['Identidade', 'Habilitação', 'Documento do Veículo',
+                                         'Carteira de Vacinação', 'Passaporte', 'Título de Eleitor', 'Outros'] }
   has_one_attached :front
   has_one_attached :back
 end
