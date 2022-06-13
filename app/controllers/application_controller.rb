70e71eb28c66ebaea8c3fb450cbd40526f4696c5
igor martins <igordavillamartins@outlook.com>
2022-06-13 11:29:22 -0300

    add genre to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name birthday cpf])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name birthday cpf genre])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name birthday cpf])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name birthday cpf genre])
   end
 
   private
